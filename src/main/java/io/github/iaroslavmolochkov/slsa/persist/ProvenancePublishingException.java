06601d6ec48dd55892808964d13cd20befd807fa
Yaroslav Molochkov <iaroslav.molochkov@gmail.com>
2026-09-05 23:20:09 +0200

    chore: formatting

--- src/main/java/io/github/iaroslavmolochkov/slsa/persist/ProvenancePublishingException.java
+++ src/main/java/io/github/iaroslavmolochkov/slsa/persist/ProvenancePublishingException.java
@@ -5,5 +5,5 @@ public class ProvenancePublishingException extends RuntimeException {
     public ProvenancePublishingException(String message, Exception ex) {
         super(message, ex);
     }
-    
+
 }
